21b73af623459cbbf6fa038803c490781d88cbbd
Iparigl784 <iparigl784@g.educaand.es>
2025-11-27 20:23:23 +0100

    Navbar hecho

--- src/components/Reusables/Header.jsx
+++ src/components/Reusables/Header.jsx
@@ -1,13 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { UserContext } from "../../contexts/ContextUser";
 import { useContext } from "react";
 import { signOutUser } from "../../utils/firebase";
 import logo from "/img/logo.webp";
 import buscar from "/img/buscar.webp";
 import usuario from "/img/usuario.webp";
-import carrito from "/img/carrito.webp"; 
+import carrito from "/img/carrito.webp";
 import styles from "../../pages/modules/header.module.css";
-
 
 function Header() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -21,18 +20,47 @@ function Header() {
     }
   };
   return (
-    <header className={styles.header}>
-      <div className={styles.busquedaContainer}>
-        <img src={buscar} alt=""/>
-      </div>
-      <div className={styles["logoContainer"]}>
-        <img src={logo} />
-      </div>
-      <div className={styles["adminContainer"]}>
-        <img src={usuario} />
-        <img src={carrito}/>
-      </div>
-    </header>
+    <>
+      <header className={styles.header}>
+        <div className={styles.busquedaContainer}>
+          <img src={buscar} alt="" />
+        </div>
+        <div className={styles["logoContainer"]}>
+          <img src={logo} />
+        </div>
+        <div className={styles["adminContainer"]}>
+          <img src={usuario} />
+          <img src={carrito} />
+        </div>
+      </header>
+      <nav className={styles.navbar}>
+        <div className={styles.navContainer}>
+          <ul>
+            <li>
+              <NavLink to="/Camiseta">Camisetas de Fútbol</NavLink>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <NavLink to="/CajasMisteriosas">Cajas Misteriosas</NavLink>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <NavLink to="/Nosotros">Nosotros</NavLink>
+            </li>
+            <li>
+              <span>|</span>
+            </li>
+            <li>
+              <NavLink to="/Contacto">Contacto</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
