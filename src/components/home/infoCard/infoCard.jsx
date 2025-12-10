69dd1f2e6395fb66b57da4a9dd8e0dc8420c66ed
Iván <ivandarioparraiglesias@gmail.com>
2025-12-10 14:29:42 +0100

    Componente InfoCard

---
+++ src/components/home/infoCard/infoCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./InfoCard.module.css";
+import { Icon } from "@iconify/react";
+
+const InfoCard = ({ icon, text}) => {
+  return (
+    <div className={styles.infoCard}>
+      <Icon icon={icon}/>
+      <h3>{text}</h3>
+    </div>
+  );
+};
+
+export default InfoCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,18 @@ import styles from "./modules/home.module.css"
 import Carousel from "../components/home/carousel/carousel"
 import CardGrid from "../components/home/cardGrid/cardGrid"
 import Marquee from "../components/home/marquee/marquee"
+import InfoCard from "../components/home/infoCard/infoCard";
 
 function Home() {
   return (
     <div className={styles.container}>
       <Carousel />
+      <div className={styles.infoCardsContainer}>
+        <InfoCard
+          icon="fluent-mdl2:giftbox"
+          text="Elige tu caja sorpresa"
+        />
+      </div>
       <CardGrid />
       <Marquee />
     </div>
