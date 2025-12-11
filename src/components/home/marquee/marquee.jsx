aaae13618ad59e64eb591a5fea5096849f1ace38
Iparigl784 <iparigl784@g.educaand.es>
2025-12-11 20:27:32 +0100

    Modificación marquee

--- src/components/home/marquee/marquee.jsx
+++ src/components/home/marquee/marquee.jsx
@@ -1,29 +1,34 @@
 import styles from "./marquee.module.css";
 
 export default function Marquee() {
-  const logos = [
-    "/img/marquee/madrid.svg",
-    "/img/marquee/barca.webp",
-    "/img/marquee/atletico.webp",
-    "/img/marquee/bayern.webp",
-    "/img/marquee/chelsea.webp",
-    "/img/marquee/psg.webp",
-    "/img/marquee/united.png",
-    "/img/marquee/liverpool.webp",
-    "/img/marquee/malaga.png",
-    "/img/marquee/city.png",
-    "/img/marquee/juve.webp"
-  ];
+  // Importa automáticamente TODAS las imágenes del directorio
+  const images = import.meta.glob(
+    "/public/img/marquee/*.{png,jpg,jpeg,webp,svg}",
+    {
+      eager: true,
+    }
+  );
 
-  // Duplicamos los logos para scroll continuo
+  // Convertimos el objeto en un array de rutas
+  const logos = Object.values(images).map((img) => img.default);
+
+  // Scroll infinito
   const logosLoop = [...logos, ...logos];
 
   return (
     <div className={styles.marquee}>
       <div className={styles.marquee__track}>
-        {logosLoop.map((logo, index) => (
+        {/* Primera tanda */}
+        {logos.map((logo, index) => (
           <div className={styles.marquee__item} key={index}>
             <img src={logo} alt={`Logo ${index}`} />
+          </div>
+        ))}
+
+        {/* Segunda tanda (copia exacta) */}
+        {logos.map((logo, index) => (
+          <div className={styles.marquee__item} key={`copy-${index}`}>
+            <img src={logo} alt={`Logo copia ${index}`} />
           </div>
         ))}
       </div>
